76163a57e83e469e1cdc64d878002a97344b6ecb
surajbhatt27 <bhattsuraj0066@gmail.com>
2024-01-02 10:54:36 +0530

    new program added

---
+++ Circular-L.L_reverse/main.c
@@ -0,0 +1,77 @@
+//program to revese circular list.
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+struct node *temp, *newnode, *tail;
+
+//function to create a list.
+void create()
+{
+  int a;
+  tail = NULL;
+  do 
+  {
+    newnode = (struct node*)malloc(sizeof(struct node));
+    printf("Enter the data: ");
+    scanf("%d", &newnode -> data);
+    if(tail == NULL)
+    {
+        tail = newnode;
+        tail -> next = newnode;
+    }
+    else
+    {
+        newnode -> next = tail -> next;
+        tail -> next = newnode;
+    }
+    tail = newnode;
+    printf("Enter 1 for adding another node: ");
+    scanf("%d", &a);
+  }while(a == 1);
+}
+
+//function to display list.
+void display()
+{
+    temp = tail -> next;
+    do
+    {
+        printf("%d ", temp -> data);
+        temp = temp -> next;
+    } while (temp != tail -> next);
+}
+
+//function to reverse the list.
+void reverse()
+{
+    struct node *prevNode, *currentNode, *nextNode;
+    currentNode = tail -> next;
+    nextNode = currentNode -> next;
+    while (currentNode != tail)
+    {
+        prevNode = currentNode;
+        currentNode = nextNode;
+        nextNode = nextNode -> next;
+        currentNode -> next = prevNode;
+    }
+    nextNode -> next = tail;
+    tail = nextNode;
+}
+
+int main()
+{
+   printf("Let's create a Circular Linked list \n");
+   create();
+   printf("The list is : ");
+   display();
+   printf("\nReversed list: ");
+   reverse();
+   display();
+   return 0;
+}
